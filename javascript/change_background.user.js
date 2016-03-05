e3886d8e68955dd405e14875a523bce5892398a1
Chunis Deng <chunchengfh@gmail.com>
2016-03-05 23:56:04 +0800

    add greasemonkey script for changing webpage backgroud color

---
+++ javascript/change_background.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name          修改网页背景色
+// @description   修改网页的背景颜色
+// @author        chunchengfh
+//
+// @include       *://mitpress.mit.edu/sicp/full-text/book/*
+//
+// @grant         unsafeWindow
+// @encoding      utf-8
+// @run-at        document-idle
+//
+// ==/UserScript==
+
+
+document.body.style.background="#D9D9D9";
