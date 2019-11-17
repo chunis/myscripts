2650eb29f05eb6fef3b8d1d4ba3aca76ca47d4dc
Chunis Deng <chunchengfh@gmail.com>
2019-11-17 11:45:54 +0800

    add usage() and test words instead of only characters

--- python/learn_jp_pronouncation.py
+++ python/learn_jp_pronouncation.py
@@ -33,6 +33,14 @@ allsounds = [
     'わ', 'ワ', 'wa', 'い', 'イ', 'i  ', 'う', 'ウ', 'u  ', 'え', 'エ', 'e ', 'を', 'ヲ', 'o ',
 ]
 
+def usage(name):
+    print '''Usage:
+    %s          : use 1st ('あ') as hint
+    %s 1        : use 1st ('あ') as hint
+    %s 2        : use 2nd ('ア') as hint
+    %s 3        : use 3rd ('a') as hint
+    %s 4 type n : 'n' number of 'type' words''' %(name, name, name, name, name)
+
 def shuffle_tuple_as_list(tpl):
     lst = list(tpl)
     random.shuffle(lst)
@@ -57,43 +65,90 @@ def check_a_sound(tpl, index, newset):
             break
 
 
-option = 1
-if len(sys.argv) > 1:
-    option = int(sys.argv[1])
-    if option > 0 and option <= 3:
-        pass
-    else:
-        option = 1
+def test_single(snd_list, option):
+    tmp_set = set()
+    while snd_list:
+        print "Let's begin..."
+        for i, x in enumerate(snd_list):
+            print "(%d/%d): " %(i+1, len(snd_list)),
+            check_a_sound(x, option, tmp_set)
+
+        # Finish one loop. Show what's left
+        print "Totally %d items left" %len(tmp_set)
+        _more = raw_input("Show all? [y]")
+        more = _more.strip().lower()
+        if more == "n":
+            pass
+        else:
+            for x in tmp_set:
+                print x[0], x[1], x[2]
+            print
+
+        snd_list = shuffle_tuple_as_list(tmp_set)
+        tmp_set = set()
+
+def test_multi(sound_list, index, num):
+    loop = 25  # one test with 25 words
+    indexlist = range(len(sound_list))
+    i = 0
+    while i < loop:
+        random.shuffle(indexlist)
+        print "(%d/%d) " %(i+1, loop),
+        tmplist = [sound_list[si] for si in indexlist[:num]]
+        for ix in tmplist:
+            print ix[index-1],
+        print
+
+        while True:
+            feedback = raw_input()
+            feedback = feedback.strip().lower()
+            if feedback == 'q':
+                print "Exit... Bye"
+                sys.exit()
+            elif feedback == 'l':
+                for n in range(3):
+                    for ix in tmplist:
+                        print "%s" %ix[n],
+                    print
+            elif feedback == 'y':
+                i += 1
+                break
+            elif feedback == 'n':
+                break
+
 
 snd_set = set()
 while allsounds:
     snd_set.add(tuple(allsounds[:3]))
     allsounds = allsounds[3:]
-snd_list = shuffle_tuple_as_list(snd_set)
+sound_list = shuffle_tuple_as_list(snd_set)
+
+
+option = 1
+index = 1
+num = 3
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == '-h' or sys.argv[1] == '--help':
+        usage(sys.argv[0])
+        sys.exit()
+
+    option = int(sys.argv[1])
+    if option > 0 and option <= 3:
+        pass
+    elif option == 4:
+        index = int(sys.argv[2])
+        num = int(sys.argv[3])
+    else:
+        option = 1
+
 
 start = time.ctime()
 print "\nStart at: %s\n" %start
-
-tmp_set = set()
-while snd_list:
-    print "Let's begin..."
-    for i, x in enumerate(snd_list):
-        print "(%d/%d): " %(i+1, len(snd_list)),
-        check_a_sound(x, option, tmp_set)
-
-    # Finish one loop. Show what's left
-    print "Totally %d items left" %len(tmp_set)
-    _more = raw_input("Show all? [y]")
-    more = _more.strip().lower()
-    if more == "n":
-        pass
-    else:
-        for x in tmp_set:
-            print x[0], x[1], x[2]
-        print
-
-    snd_list = shuffle_tuple_as_list(tmp_set)
-    tmp_set = set()
+if option < 3:
+    test_single(sound_list, option)
+elif option == 4:
+    test_multi(sound_list, index, num)
 
 print "Start  at:", start
 print "Finish at:", time.ctime(), "\n"
